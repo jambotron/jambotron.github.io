40eafce2c82461337d5a594763c111eb95182462
jambotron <nekipelov2@gmail.com>
2019-01-21 20:25:20 +0200

    Initial commit

---
+++ precache-manifest.4e5a51691aa779059a3ad3129b0a2dae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5685a1b4a1994719957b",
+    "url": "/static/js/main.5685a1b4.chunk.js"
+  },
+  {
+    "revision": "920219f12c0a549bd234",
+    "url": "/static/js/1.920219f1.chunk.js"
+  },
+  {
+    "revision": "5685a1b4a1994719957b",
+    "url": "/static/css/main.2171a12d.chunk.css"
+  },
+  {
+    "revision": "920219f12c0a549bd234",
+    "url": "/static/css/1.d74872e9.chunk.css"
+  },
+  {
+    "revision": "70944409072770c14275ac130ae958d8",
+    "url": "/index.html"
+  }
+];
